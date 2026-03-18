dbbd48dfdcb64c0d6613a8a1c55de48ca6a5ca61
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-03-18 14:22:46 +0000

    Add SNS and S3 clients and fix dependency issues #patch

---
+++ src/common/helpers/sns-client.js
@@ -0,0 +1,56 @@
+import { PublishCommand, SNSClient } from '@aws-sdk/client-sns'
+
+let defaultTopic
+let loggerInstance
+let clientSetup = false
+
+let snsClient
+
+export const setupClient = (region, awsEndpointUrl, logger, publishToTopic) => {
+  snsClient = new SNSClient({
+    region,
+    endpoint: awsEndpointUrl
+  })
+  loggerInstance = logger
+  defaultTopic = publishToTopic
+  clientSetup = true
+}
+
+export const publishMessage = async (
+  data,
+  messageAttributes = {},
+  topic = defaultTopic
+) => {
+  if (!clientSetup) {
+    throw new Error(
+      'SNS client not setup. Call setupClient() before publishing messages.'
+    )
+  }
+  loggerInstance.info(`Publish command ${topic}`)
+  await snsClient.send(
+    new PublishCommand({
+      TopicArn: topic,
+      Message: JSON.stringify(data),
+      MessageAttributes: convertMessageAttributes(messageAttributes)
+    })
+  )
+}
+
+/* Converts a simple key-value object into the format required by SNS for message attributes.
+  Note only supports string values currently
+ Example input: { key1: "value1", key2: "value2" }
+ Example output: {
+   key1: { DataType: "String", StringValue: "value1" },
+   key2: { DataType: "String", StringValue: "value2" }
+ }
+*/
+const convertMessageAttributes = (attributes) => {
+  const convertedAttributes = {}
+  for (const key in attributes) {
+    convertedAttributes[key] = {
+      DataType: 'String',
+      StringValue: attributes[key]
+    }
+  }
+  return convertedAttributes
+}
