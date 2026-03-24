929d4e98d24b1e7a8dad89ccb1a87ef839ccd454
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-03-24 16:06:31 +0000

    sonar...

--- src/utils/get-service-version.js
+++ src/utils/get-service-version.js
@@ -1,9 +1,10 @@
 import { config } from '../config.js'
 
+const LOCAL_SUFFIX_LIMIT = 9999
 export const getServiceVersion = () => {
   let serviceVersion = config.get('serviceVersion')
   if (config.get('cdpEnvironment') === 'local') {
-    serviceVersion += `-${Math.floor(Math.random() * 9999)}`
+    serviceVersion += `-${Math.floor(Math.random() * LOCAL_SUFFIX_LIMIT)}`
   }
   return serviceVersion
 }
