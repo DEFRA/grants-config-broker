3d0b0fd1dff69bbb20b51031ae6efa165645f10c
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2026-05-26 08:48:30 +0100

    docs: Add async api documentation, and endpoint to browse it #patch (#89)

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import neostandard from 'neostandard'
 
 export default neostandard({
   env: ['node', 'vitest'],
-  ignores: [...neostandard.resolveIgnoresFromGitignore()],
+  ignores: [
+    ...neostandard.resolveIgnoresFromGitignore(),
+    'src/routes/asyncapidocs/*'
+  ],
   noJsx: true,
   noStyle: true
 })
