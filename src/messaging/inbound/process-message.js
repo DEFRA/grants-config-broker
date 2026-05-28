6fae037985ca4642bcdae924b0fcced11dd7d8fe
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2026-05-28 08:40:15 +0100

    fix(GRAN-40): Validate incoming list of files and handle missing #patch (#93)
    Also upgrade unidici dependency

--- src/messaging/inbound/process-message.js
+++ src/messaging/inbound/process-message.js
@@ -1,11 +1,17 @@
 import { considerRelease } from '../../deploy-version.js'
 import { getServiceVersion } from '../../utils/get-service-version.js'
 import { notifyVersion } from '../outbound/notify-version.js'
+import { listFiles } from '../../storage/s3-interactions.js'
 
 export const processInputMessage = async (message, db, logger, _attributes) => {
   try {
     const { grant, version, status, files } = message
-    // TODO: Could add some validation here to check the files are in S3 in expected structure, but for now, assume there and OK
+
+    logger.info(
+      `Received Config Input request for grant: ${grant}, version: ${version}`
+    )
+    await validateFilesInS3(grant, version, files, logger)
+
     const releaseInfo = await considerRelease(
       logger,
       db,
@@ -21,5 +27,17 @@ export const processInputMessage = async (message, db, logger, _attributes) => {
     }
   } catch (err) {
     logger.error(err, 'Unable to process Input request:')
+  }
+}
+
+const validateFilesInS3 = async (grant, version, files, logger) => {
+  const configFiles = await listFiles(logger, `${grant}/${version}`)
+  const missingFiles = files.filter(
+    (fileName) => !configFiles.some((file) => file.Key === fileName)
+  )
+  if (missingFiles.length > 0) {
+    throw new Error(
+      `Missing files in S3, cannot publish this config version: ${missingFiles.join(', ')}`
+    )
   }
 }
--- src/messaging/inbound/process-message.test.js
+++ src/messaging/inbound/process-message.test.js
@@ -1,4 +1,5 @@
 import { processInputMessage } from './process-message.js'
+import { listFiles } from '../../storage/s3-interactions.js'
 import { considerRelease } from '../../deploy-version.js'
 import { notifyVersion } from '../outbound/notify-version.js'
 import { getServiceVersion } from '../../utils/get-service-version.js'
@@ -6,6 +7,7 @@ import { getServiceVersion } from '../../utils/get-service-version.js'
 vi.mock('../../deploy-version.js')
 vi.mock('../../utils/get-service-version.js')
 vi.mock('../outbound/notify-version.js')
+vi.mock('../../storage/s3-interactions.js')
 
 describe('Process Message test', () => {
   const mockDb = {}
@@ -18,16 +20,18 @@ describe('Process Message test', () => {
   beforeEach(async () => {
     vi.clearAllMocks()
     getServiceVersion.mockReturnValueOnce('1.0.0')
+    listFiles.mockResolvedValue([])
   })
 
   it('should call considerRelease but no further action when version not released', async () => {
+    listFiles.mockResolvedValueOnce([{ Key: 'some-grant/1.0.0/some/file.txt' }])
     considerRelease.mockResolvedValueOnce(null)
     await processInputMessage(
       {
         grant: 'some-grant',
         status: 'draft',
         version: '1.0.0',
-        files: ['some/file.txt']
+        files: ['some-grant/1.0.0/some/file.txt']
       },
       mockDb,
       mockLogger
@@ -39,17 +43,18 @@ describe('Process Message test', () => {
       { name: 'some-grant', version: '1.0.0' },
       '1.0.0',
       'draft',
-      ['some/file.txt']
+      ['some-grant/1.0.0/some/file.txt']
     )
     expect(notifyVersion).not.toHaveBeenCalled()
   })
 
   it('should send notification when version is released', async () => {
+    listFiles.mockResolvedValueOnce([{ Key: 'some-grant/1.0.0/some/file.txt' }])
     const releaseInfo = {
       grant: 'some-grant',
       version: '1.0.0',
       status: 'draft',
-      manifest: ['some/file.txt'],
+      manifest: ['some-grant/1.0.0/some/file.txt'],
       updatedInBrokerVersion: '1.0.0',
       createdInBrokerVersion: '1.0.0',
       lastUpdated: new Date(),
@@ -64,7 +69,7 @@ describe('Process Message test', () => {
         grant: 'some-grant',
         status: 'draft',
         version: '1.0.0',
-        files: ['some/file.txt']
+        files: ['some-grant/1.0.0/some/file.txt']
       },
       mockDb,
       mockLogger
@@ -76,18 +81,19 @@ describe('Process Message test', () => {
       { name: 'some-grant', version: '1.0.0' },
       '1.0.0',
       'draft',
-      ['some/file.txt']
+      ['some-grant/1.0.0/some/file.txt']
     )
     expect(notifyVersion).toHaveBeenCalledWith(releaseInfo, mockLogger)
   })
 
   it('should default status to draft if not specified', async () => {
+    listFiles.mockResolvedValueOnce([{ Key: 'some-grant/1.0.0/some/file.txt' }])
     considerRelease.mockResolvedValueOnce(null)
     await processInputMessage(
       {
         grant: 'some-grant',
         version: '1.0.0',
-        files: ['some/file.txt']
+        files: ['some-grant/1.0.0/some/file.txt']
       },
       mockDb,
       mockLogger
@@ -98,19 +104,72 @@ describe('Process Message test', () => {
       { name: 'some-grant', version: '1.0.0' },
       '1.0.0',
       'draft',
-      ['some/file.txt']
+      ['some-grant/1.0.0/some/file.txt']
     )
   })
 
   it('should catch and log an error if thrown', async () => {
-    considerRelease.mockThrowOnce(new Error('Some error'))
+    listFiles.mockResolvedValueOnce([{ Key: 'some-grant/1.0.0/some/file.txt' }])
+    considerRelease.mockRejectedValueOnce(new Error('Some error'))
 
-    await processInputMessage({}, mockDb, mockLogger)
+    await processInputMessage(
+      {
+        grant: 'some-grant',
+        version: '1.0.0',
+        files: ['some-grant/1.0.0/some/file.txt']
+      },
+      mockDb,
+      mockLogger
+    )
 
     expect(considerRelease).toHaveBeenCalled()
     expect(notifyVersion).not.toHaveBeenCalled()
     expect(mockLogger.error).toHaveBeenCalledWith(
       new Error('Some error'),
+      'Unable to process Input request:'
+    )
+  })
+
+  it('should throw error and log it if files are missing in S3', async () => {
+    listFiles.mockResolvedValueOnce([{ Key: 'some-grant/1.0.0/file1.txt' }])
+
+    await processInputMessage(
+      {
+        grant: 'some-grant',
+        version: '1.0.0',
+        files: ['some-grant/1.0.0/file1.txt', 'some-grant/1.0.0/file2.txt']
+      },
+      mockDb,
+      mockLogger
+    )
+
+    expect(listFiles).toHaveBeenCalledWith(mockLogger, 'some-grant/1.0.0')
+    expect(considerRelease).not.toHaveBeenCalled()
+    expect(mockLogger.error).toHaveBeenCalledWith(
+      new Error(
+        'Missing files in S3, cannot publish this config version: some-grant/1.0.0/file2.txt'
+      ),
+      'Unable to process Input request:'
+    )
+  })
+
+  it('should throw error and log it if no files are found in S3', async () => {
+    listFiles.mockResolvedValueOnce([])
+
+    await processInputMessage(
+      {
+        grant: 'some-grant',
+        version: '1.0.0',
+        files: ['file1.txt']
+      },
+      mockDb,
+      mockLogger
+    )
+
+    expect(mockLogger.error).toHaveBeenCalledWith(
+      new Error(
+        'Missing files in S3, cannot publish this config version: file1.txt'
+      ),
       'Unable to process Input request:'
     )
   })
