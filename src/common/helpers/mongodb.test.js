0231748c257f44a74fe3ab1d144e184f4d987ca0
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-06-17 16:12:55 +0100

    fix: Point to new grant locations. Update dependencies, fix flaky test. #patch

--- src/common/helpers/mongodb.test.js
+++ src/common/helpers/mongodb.test.js
@@ -41,7 +41,7 @@ describe('#mongoDb', () => {
       const closeSpy = vi.spyOn(server.mongoClient, 'close')
       await server.stop({ timeout: 1000 })
 
-      expect(closeSpy).toHaveBeenCalledWith(true)
+      expect(closeSpy).toHaveBeenCalled()
     })
   })
 })
