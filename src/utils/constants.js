e27addd47db670ad061875109ba6df54031fd43c
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2026-08-14 09:36:21 +0200

    fix(GRAN-123): Set correct feature controls subject name #patch (#191)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const DRAFT_STATUS = 'draft'
 
 export const LOCAL_SUBJECT = 'local'
 export const BROWSER_SUBJECT = 'grants-config-browser'
-export const FEATURE_CONTROL_SUBJECT = 'grants-config-feature-control'
+export const FEATURE_CONTROL_SUBJECT = 'grants-config-feature-controls'
